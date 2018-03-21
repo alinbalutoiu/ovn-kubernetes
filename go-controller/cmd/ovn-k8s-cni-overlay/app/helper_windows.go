47ec8bff9d207c4403d25d6c9a1ac625930ef794
Alin-Gheorghe Balutoiu <abalutoiu@cloudbasesolutions.com>
2018-03-21 09:40:17 +0100

    ovn-k8s-cni-overlay: Add Windows Server 2016 support
    This patch adds Windows Server 2016 build 14393.
    
    Kubernetes supports only one container per pod in this mode.
    Also, this version does not support setting custom IP/MAC
    address. The CNI will update the port from OVN with the
    assigned IP/MAC address.
    
    Signed-off-by: Alin Balutoiu <abalutoiu@cloudbasesolutions.com>

--- go-controller/cmd/ovn-k8s-cni-overlay/app/helper_windows.go
+++ go-controller/cmd/ovn-k8s-cni-overlay/app/helper_windows.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -16,15 +17,19 @@ import (
 	"github.com/containernetworking/cni/pkg/types/current"
 
 	"github.com/openvswitch/ovn-kubernetes/go-controller/pkg/config"
+	"github.com/openvswitch/ovn-kubernetes/go-controller/pkg/util"
 )
 
 // noNameNetNS - This is received for the infra container, in this case we
 // have to create the network endpoint and attach it to the infra container
 // containerNetNS - This is received for all the other containers from
 // the same pod, do not create the network endpoint in this case
+// windowsVersion2016 - This is the latest windows version which does not support
+// multiple containers per pod. The CNI will act differently on this version
 const (
-	noNameNetNS    = "none"
-	containerNetNS = "container:"
+	noNameNetNS        = "none"
+	containerNetNS     = "container:"
+	windowsVersion2016 = 14393
 )
 
 // More details about the above constants can be found in the following PR:
@@ -142,20 +147,19 @@ func ConfigureInterface(args *skel.CmdArgs, namespace string, podName string, ma
 		return nil, err
 	}
 
-	// Ensure that the macAddress is given in xx:xx:xx:xx:xx:xx format
-	macAddressIpFormat := strings.Replace(macAddress, ":", "-", -1)
-
 	// Check if endpoint is created, otherwise create it.
 	// This is to make the call to add idempotent
 	var createdEndpoint *hcsshim.HNSEndpoint
 	createdEndpoint, err = hcsshim.GetHNSEndpointByName(endpointName)
 	if err != nil {
 		logrus.Infof("HNS endpoint %q does not exist", endpointName)
+		// macAddress for the endpoint should have xx-xx-xx-xx-xx-xx format
+		macAddressEndpoint := strings.Replace(macAddress, ":", "-", -1)
 		hnsEndpoint := &hcsshim.HNSEndpoint{
 			Name:           endpointName,
 			VirtualNetwork: hnsNetworkId,
 			IPAddress:      ipAddr,
-			MacAddress:     macAddressIpFormat,
+			MacAddress:     macAddressEndpoint,
 			PrefixLength:   uint8(ipMaskSize),
 		}
 		createdEndpoint, err = createHNSEndpoint(hnsEndpoint)
@@ -199,8 +203,28 @@ func ConfigureInterface(args *skel.CmdArgs, namespace string, podName string, ma
 
 // PlatformSpecificCleanup deletes the OVS port and also the corresponding
 // HNS Endpoint for the OVS port.
-func PlatformSpecificCleanup(args *skel.CmdArgs) error {
+func PlatformSpecificCleanup(args *skel.CmdArgs, argsMap map[string]string) error {
+	// argsMap is used only for Windows Server 2016 build number 14393
 	endpointName := args.ContainerID
+	windowsVersion, err := getWindowsVersion()
+	if err == nil && windowsVersion <= windowsVersion2016 {
+		namespace := argsMap["K8S_POD_NAMESPACE"]
+		podName := argsMap["K8S_POD_NAME"]
+		ifaceID := fmt.Sprintf("%s_%s", namespace, podName)
+		ovsArgs := []string{
+			"--bare", "find", "interface", fmt.Sprintf("external_ids:iface-id=%s", ifaceID),
+		}
+		outputFind, errFind := exec.Command("ovs-vsctl", ovsArgs...).CombinedOutput()
+		if errFind == nil && len(outputFind) != 0 {
+			outputFindList := strings.Split(string(outputFind), "Container NIC ")
+			endpointName = fmt.Sprintf("Container NIC %s", strings.Split(outputFindList[1], "\r\n")[0])
+			logrus.Infof("Found container OVS port: %v", endpointName)
+		} else {
+			logrus.Infof("Infra container OVS port not found")
+			return nil
+		}
+	}
+
 	ovsArgs := []string{
 		"del-port", "br-int", endpointName,
 	}
@@ -209,31 +233,214 @@ func PlatformSpecificCleanup(args *skel.CmdArgs) error {
 		// DEL should be idempotent; don't return an error just log it
 		logrus.Warningf("failed to delete OVS port %s: %v  %q", endpointName, err, string(out))
 	}
+	if windowsVersion <= windowsVersion2016 {
+		// On Windows Server 2016 we don't have to take care of the HNS Endpoint
+		return nil
+	}
 	// Return the error if we can't delete the HNS Endpoint, we don't want any leaks
 	return deleteHNSEndpoint(endpointName)
 }
 
+func getFakeCNIResult() *current.Result {
+	fakeAddr, fakeAddrNet, _ := net.ParseCIDR("1.2.3.4/32")
+	fakeResult := &current.Result{
+		IPs: []*current.IPConfig{
+			{
+				Version:   "4",
+				Interface: current.Int(1),
+				Address:   net.IPNet{IP: fakeAddr, Mask: fakeAddrNet.Mask},
+				Gateway:   net.ParseIP("1.2.3.4"),
+			},
+		},
+	}
+	return fakeResult
+}
+
 // InitialPlatformCheck checks to see if the container is an infra container
 // by looking at the namespace prefix. If it's not, then it has nothing to
-// do and the CNI should stop here.
-func InitialPlatformCheck(args *skel.CmdArgs) (bool, *current.Result) {
+// do and the CNI should stop here for Windows Server version greated than windowsVersion2016.
+// For Windows Server version windowsVersion2016 it will configure the pod interface.
+func InitialPlatformCheck(args *skel.CmdArgs, argsMap map[string]string) (bool, *current.Result, error) {
+	windowsVersion, err := getWindowsVersion()
+	if err == nil && windowsVersion <= windowsVersion2016 {
+		namespace := argsMap["K8S_POD_NAMESPACE"]
+		podName := argsMap["K8S_POD_NAME"]
+		ifaceID := fmt.Sprintf("%s_%s", namespace, podName)
+		if strings.Compare(args.Netns, noNameNetNS) != 0 && !checkContainerOVSPort(ifaceID) {
+			result, err := configureInterface2016(args, argsMap)
+			return true, result, err
+		} else {
+			logrus.Infof("InitialPlatformCheck: CNI has nothing to do (called for infra container or network has been setup")
+			return true, getFakeCNIResult(), nil
+		}
+
+	}
 	if strings.HasPrefix(args.Netns, containerNetNS) || strings.Compare(args.Netns, noNameNetNS) != 0 {
 		// If it is a normal container from the pod, there is nothing to do.
 		// Also, if it is not an infra container, nothing to do in this case as well.
-		logrus.Infof("InitialPlatformCheck: CNI called for normal container or infra container, nothing to do")
+		logrus.Infof("InitialPlatformCheck: CNI has nothing to do (the container is not the infra container)")
 		// This result is ignored anyway by Kubernetes.
-		fakeAddr, fakeAddrNet, _ := net.ParseCIDR("1.2.3.4/32")
-		fakeResult := &current.Result{
-			IPs: []*current.IPConfig{
-				{
-					Version:   "4",
-					Interface: current.Int(1),
-					Address:   net.IPNet{IP: fakeAddr, Mask: fakeAddrNet.Mask},
-					Gateway:   net.ParseIP("1.2.3.4"),
-				},
-			},
-		}
-		return true, fakeResult
+		return true, getFakeCNIResult(), nil
 	}
-	return false, &current.Result{}
+	return false, &current.Result{}, nil
+}
+
+// Check if the OVS port for the container interface has been already set up
+func checkContainerOVSPort(ifaceID string) bool {
+	ovsArgs := []string{
+		"find", "interface", fmt.Sprintf("external_ids:iface-id=%s", ifaceID),
+	}
+	outputFind, err := exec.Command("ovs-vsctl", ovsArgs...).CombinedOutput()
+	if err == nil && len(outputFind) != 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Retrieves the Windows build number using wmic
+func getWindowsVersion() (int, error) {
+	args := []string{"os", "get", "BuildNumber", "/value"}
+	out, err := exec.Command("wmic", args...).Output()
+	if err != nil {
+		return 0, err
+	}
+	buildInfo := strings.Split(string(strings.Trim(string(out), "\" \n\r")), "=")
+	windowsVersion, err := strconv.Atoi(buildInfo[1])
+	if err != nil {
+		return 0, err
+	}
+	logrus.Debugf("Windows Version detected: %d", windowsVersion)
+	return windowsVersion, nil
+}
+
+func runWMIC(namespace string, class string, whereClause string, propertyToRetrieve string) (string, error) {
+	cmd := "wmic"
+	query := []string{fmt.Sprintf("/namespace:\\\\%s", namespace),
+		"Path", class,
+		"WHERE", whereClause,
+		"GET", propertyToRetrieve,
+		"/format:list",
+	}
+	logrus.Infof("Running wmic query: 'wmic %v'", query)
+	wmicOutput, err := exec.Command(cmd, query...).Output()
+	if err != nil {
+		return "", err
+	}
+	trimmedOutput := strings.TrimSpace(string(wmicOutput))
+	wmicOutputList := strings.Split(strings.Trim(trimmedOutput, "\""), "=")
+	if len(wmicOutputList) == 1 {
+		// wmic will not return any error if there are no instances available, the output will not contain
+		// any '=' and the length of the list should be 1
+		return "", fmt.Errorf("wmic returned no instances available")
+	}
+	return wmicOutputList[1], nil
+}
+
+func getContainerInterfaceName(containerID string) (string, error) {
+	compGUID, err := runWMIC("root\\standardcimv2", "MSFT_NetCompartment",
+		fmt.Sprintf("CompartmentDescription='\\\\Container_%s'", containerID),
+		"CompartmentGuid")
+	if err != nil {
+		return "", err
+	}
+
+	portElementName, err := runWMIC("root\\virtualization\\v2", "Msvm_EthernetPortAllocationSettingData",
+		fmt.Sprintf("CompartmentGuid='%s'", compGUID),
+		"ElementName")
+	if err != nil {
+		return "", err
+	}
+	portElementNameList := strings.Split(string(portElementName), " ")
+	containerInterfaceName := fmt.Sprintf("Container NIC %v", portElementNameList[len(portElementNameList)-1])
+	return containerInterfaceName, nil
+}
+
+func getContainerPortInfo(containerInterfaceName string) (string, string, string, error) {
+	macAddress, err := runWMIC("root\\virtualization\\v2", "Msvm_LanEndpoint",
+		fmt.Sprintf("ElementName='%s' and MACAddress is not null", containerInterfaceName),
+		"MACAddress")
+	if err != nil {
+		return "", "", "", err
+	}
+	// macAddress will have format XXXXXXXXXXXX, we want XX-XX-XX-XX-XX-XX
+	for i := 2; i < len(macAddress); i += 3 {
+		macAddress = macAddress[:i] + "-" + macAddress[i:]
+	}
+	logrus.Infof("Found MAC Address %v for port %v", macAddress, containerInterfaceName)
+	endpointList, err := hcsshim.HNSListEndpointRequest()
+	if err != nil {
+		return "", "", "", err
+	}
+	ipAddress := ""
+	gatewayIP := ""
+	for _, endpoint := range endpointList {
+		if strings.Compare(macAddress, endpoint.MacAddress) == 0 {
+			ipAddress = fmt.Sprintf("%s/%d", endpoint.IPAddress.String(), endpoint.PrefixLength)
+			gatewayIP = endpoint.GatewayAddress
+			break
+		}
+	}
+	if len(ipAddress) == 0 {
+		return "", "", "", fmt.Errorf("Failed to find the IP for port: %v", containerInterfaceName)
+	}
+	logrus.Infof("Found IP %v for port: %v", ipAddress, containerInterfaceName)
+	// Return the macAddress in xx:xx:xx:xx:xx:xx format
+	macAddress = strings.Replace(macAddress, "-", ":", -1)
+	macAddress = strings.ToLower(macAddress)
+	return ipAddress, macAddress, gatewayIP, nil
+}
+
+func configureInterface2016(args *skel.CmdArgs, argsMap map[string]string) (*current.Result, error) {
+	logrus.Infof("Setting up pod network on Windows Server 2016")
+	containerInterfaceName, err := getContainerInterfaceName(args.ContainerID)
+	if err != nil {
+		return &current.Result{}, fmt.Errorf("Failed to get container port details, error: %v", err)
+	}
+
+	ovsArgs := []string{
+		"--may-exist", "add-port", "br-int", containerInterfaceName, "--", "set",
+		"interface", containerInterfaceName, "type=internal"}
+	out, err := exec.Command("ovs-vsctl", ovsArgs...).CombinedOutput()
+	if err != nil {
+		return &current.Result{}, fmt.Errorf("failure in plugging pod interface: %v  %q", err, string(out))
+	}
+
+	namespace := argsMap["K8S_POD_NAMESPACE"]
+	podName := argsMap["K8S_POD_NAME"]
+	ifaceID := fmt.Sprintf("%s_%s", namespace, podName)
+	ipAddress, macAddress, gatewayIP, err := getContainerPortInfo(containerInterfaceName)
+	if err != nil {
+		return &current.Result{}, fmt.Errorf("error getting ip/mac: %v", err)
+	}
+
+	ovsArgs = []string{"--", "set", "interface", containerInterfaceName,
+		fmt.Sprintf("external_ids:attached_mac=%s", macAddress),
+		fmt.Sprintf("external_ids:iface-id=%s", ifaceID),
+		fmt.Sprintf("external_ids:ip_address=%s", ipAddress),
+	}
+	out, err = exec.Command("ovs-vsctl", ovsArgs...).CombinedOutput()
+	if err != nil {
+		return &current.Result{}, fmt.Errorf("failure in setting external_ids for pod interface: %v  %q", err, string(out))
+	}
+
+	ipAddr, ipAddrNet, _ := net.ParseCIDR(ipAddress)
+	winResult := &current.Result{
+		IPs: []*current.IPConfig{
+			{
+				Version:   "4",
+				Interface: current.Int(1),
+				Address:   net.IPNet{IP: ipAddr, Mask: ipAddrNet.Mask},
+				Gateway:   net.ParseIP(gatewayIP),
+			},
+		},
+	}
+	// Since Windows Server 2016 doesn't support custom IP/Mac address, we have to update OVN with the given IP/Mac
+	stdout, stderr, err := util.RunOVNNbctl("--", "lsp-set-addresses", ifaceID, fmt.Sprintf("%s %s", macAddress, ipAddr.String()))
+	if err != nil {
+		logrus.Errorf("Failed to set macAddress and IP address, stdout: %q, stderr: %q, error: %v", stdout, stderr, err)
+		return &current.Result{}, err
+	}
+	logrus.Infof("Finished setting up network for container %v", args.ContainerID)
+	return winResult, nil
 }
